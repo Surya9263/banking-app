3424b171df24886e1d29a5c7911c5288e559d3ef
Surya Srivastava <suryasrivastava724@gmail.com>
2023-01-28 23:22:02 +0530

    updated

--- index.js
+++ index.js
@@ -5,15 +5,9 @@ var program = new Command();
 program.argument("<username>", "user login details");
 program
   .argument("[password]", "password for user, if needed", "default")
-  .action(async (username, password) => {
+  .action((username, password) => {
     console.log("username", username);
     console.log("password", password);
-    try {
-      await User.create({ username, password });
-      
-    } catch (error) {
-      console.log(error.message);
-    }
   });
 
 program.parse(process.argv);
